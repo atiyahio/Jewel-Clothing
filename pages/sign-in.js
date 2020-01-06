4242d1f5aa5de2d31c69ee166f412cf20ee319e8
Ahmed Atiyah <ahmedmaheratiyah@gmail.com>
2020-01-06 22:27:51 +0200

    redirect if authenticated

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { connect } from 'react-redux';
+import Router from 'next/router';
 
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
 
-const SignInSignUp = () => {
+const SignInSignUp = ({ currentUser }) => {
   const StyledSignInSignUp = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
   `;
+
+  const renderSignIn = () => {
+    if (currentUser) {
+      Router.push('/');
+    } else {
+      return (
+        <StyledSignInSignUp>
+          <SignIn />
+          <SignUp />
+        </StyledSignInSignUp>
+      );
+    }
+  };
+
   return (
     <motion.div
       initial={{ y: 50 }}
@@ -18,12 +34,11 @@ const SignInSignUp = () => {
       exit={{ y: 50 }}
       className="container"
     >
-      <StyledSignInSignUp>
-        <SignIn />
-        <SignUp />
-      </StyledSignInSignUp>
+      {renderSignIn()}
     </motion.div>
   );
 };
 
-export default SignInSignUp;
+const mapStateToProps = ({ user }) => ({ currentUser: user.currentUser });
+
+export default connect(mapStateToProps)(SignInSignUp);
